a6176b98049c5f80728a22d2a03ea4f30a272729
Elizeu Vailant <el_vailant@hotmail.com>
2023-01-04 14:02:16 -0400

    atualizando arquivos

--- client/src/Paginas/Erro.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Erro() {
-  return <h1>Erro</h1>;
-}
--- client/src/Paginas/InformacaoFruta.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function InformacaoFruta() {
-  return <div>InformacaoFruta</div>;
-}
